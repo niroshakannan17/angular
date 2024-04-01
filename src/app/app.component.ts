c6c3f42fde5bd1f7a402e1a6779a53c52a1f33a1
nirosha <niroshakannan17@gmail.com>
2024-04-01 06:56:18 +0530

    my first componens creation - completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloWorldComponents } from './hello-world/hello-world.components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  //templateUrl: './app.component.html',
-  template:"<h2>Hello {{title}} ,Welcome to first angular program</h2>",
+  imports: [RouterOutlet,HelloWorldComponents],
+  templateUrl: './app-new.components.html',
+  //template:"<h2>Hello {{title}} ,Welcome to first angular program</h2>",
   styleUrl: './app.component.css'
 })
 export class AppComponent {
---
+++ src/app/hello-world/hello-world.components.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+   selector:"hello-wolrd-component-selector",
+   standalone:true,
+   templateUrl:"./hello-world.components.html",
+   styleUrl:"./hello-word.components.css"
+})
+
+export class HelloWorldComponents{
+    username ="Nirosha"
+}
